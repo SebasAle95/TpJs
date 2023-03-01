a862f304d29abebc51f67d6c8e0dccffd833fd31
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-03-01 20:45:11 -0300

    suma de 2 numeros

---
+++ js/5-suma2numeros.js
@@ -0,0 +1,4 @@
+let num1 = prompt('ingrese el primer numero');
+let num2 = prompt('ingrese el segundo numero');
+let resultado = parseInt(num1) + parseInt(num2);
+console.log ("la suma es : " +resultado)
