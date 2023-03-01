38456e5ea9ccb6bbed881ccf2bdc6f805bc80280
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-03-01 20:56:04 -0300

    el numero mayor

---
+++ js/6-2numerosmayor.js
@@ -0,0 +1,7 @@
+let num1 = prompt("ingrese un numero");
+let num2 = prompt("ingrese otro numero");
+if (num1 > num2){
+    document.write (num1)`es mayor que` (num2)
+}else if (num2 > num1){
+    document.write (num2)`es mayor que` (num1)
+}
