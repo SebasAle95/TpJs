ba6f9f5ac221691bd0de2c3da27b2787b8d80b28
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-03-01 21:28:58 -0300

    el mayor de los 3 numeros

---
+++ js/7-programa3numeros.js
@@ -0,0 +1,13 @@
+let n1 = prompt("escriba un numero");
+let n2 = prompt("escirba otro numero");
+let n3 = prompt("escriba otro numero");
+
+if (n1 > n2 && n1 > n3){
+    document.write(n1);
+}else if (n2 > n3){
+    document.write(n2)
+}else {
+    document.write (n3)
+}
+
+
